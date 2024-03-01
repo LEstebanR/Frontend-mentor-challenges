2f4bfacac87f28860f9816f1c94f8d92fe13fcaa
Luis Esteban R <leramirezca@gmail.com>
2024-03-01 17:10:41 -0500

    delete unnecesary lines

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Raleway, Open_Sans } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 import BreadCrumb from "@/components/breadcrumbs";
